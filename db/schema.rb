aa03c7ba0c3d20fe787d2499cf9ad3d56b1d5198
gucho-n <i.m.your.dog.1192@gmail.com>
2020-09-10 19:32:43 +0900

    new_file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_10_100418) do
-
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
